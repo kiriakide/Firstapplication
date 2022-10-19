9f77f7fd238494fff7c3175bc87ef9eeceba2a82
kiriakide <kiriakigew20@gmail.com>
2022-10-20 01:07:05 +0300

    click

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
-
-def print_hi(Kiriaki):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {Kiriaki}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.title ("HealthCoach")
 
 if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
-
 
