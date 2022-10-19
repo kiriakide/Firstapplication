40021bfb4423db9a2e2bed426b4cf42a1e2d0b1c
kiriakide <kiriakigew20@gmail.com>
2022-10-20 01:31:40 +0300

    front app

--- main.py
+++ main.py
@@ -10,10 +10,3 @@ if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
-
-st.run [entrypoint file]
-st.Home.py # This is the file you run with "streamlit run"
-    # pages/
-# About.py hey # This is a page
-2_Page_two.py # This is another page
-3_😎_three.py # So is this
