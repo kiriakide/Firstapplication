d318e901afaa64a8f98447ae6e1cd361b3b9b68f
kiriakide <kiriakigew20@gmail.com>
2022-10-20 01:33:21 +0300

    front app

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Health Coach", page_icon=":red_heart", layout="wide" )
+st.set_page_config(page_title="Health Coach", page_icon=":red_heart:", layout="wide" )
 st.title ("HealthCoach")
 
 if st.button('Say hello'):
