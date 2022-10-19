04974ebf73aca525f217f57d59270dcfa117f1f1
kiriakide <kiriakigew20@gmail.com>
2022-10-20 01:18:49 +0300

    front app

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import entrypoint as entrypoint
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from numpy.core.setup_common import file
 
 st.title ("HealthCoach")
 
@@ -10,3 +11,13 @@ if st.button('Say hello'):
 else:
     st.write('Goodbye')
 
+st.run [entrypoint file]
+ class Home:
+     pass
+
+
+Home.py  # This is the file you run with "streamlit run"
+    └─── pages /
+     └─── About.py  # This is a page
+        └─── 2 _Page_two.py  # This is another page
+     └─── 3_😎_three.py  # So is this
