d24409cd9daeb8d846c4303fde2ae5ac06dacaf4
kiriakide <kiriakigew20@gmail.com>
2022-10-20 01:45:37 +0300

    front app

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide"
 
 with st.container():
     st.title ("HealthCoach :heart: :running:")
-    st.subheader()
+    st.subheader("hey")
 
 if st.button('Say hello'):
     st.write('Why hello there')
