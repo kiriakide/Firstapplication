355eb02d2e0c117eb6c442459d96fa0b99b6409b
kiriakide <kiriakigew20@gmail.com>
2022-10-30 18:59:22 +0200

    front app

--- pages/activity tracker.py
+++ pages/activity tracker.py
@@ -1,3 +1,5 @@
 import streamlit as st
+
 st.sidebar.success("select a page above")
-st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide" )
+
+st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide")
